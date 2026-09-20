f089fbb7f9a5af75a45a9358a93531b103dc3f9f
Michael <miichael.s2@gmail.com>
2026-09-20 00:31:49 -0300

    refactor(vite): migração

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   server: {
@@ -12,14 +12,12 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 4173
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
-    base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true
-    }),
     react(),
   ]
 });
